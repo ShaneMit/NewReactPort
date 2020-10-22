31b50157e545267d8ec71b50a50b830616cc9961
ShaneMit <shanemit@cox.net>
2020-10-21 20:08:04 -0700

    Updates

---
+++ precache-manifest.31b1602c11080e894c84dac3eb0dc15a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efc8e54da2117d5d0a94386a2d9f037",
+    "url": "/NewReactPort/index.html"
+  },
+  {
+    "revision": "086dcc53d7923d169c69",
+    "url": "/NewReactPort/static/js/2.776af483.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewReactPort/static/js/2.776af483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35e967631d5deb5f9a6",
+    "url": "/NewReactPort/static/js/main.f057976e.chunk.js"
+  },
+  {
+    "revision": "17e9111c400281594a63",
+    "url": "/NewReactPort/static/js/runtime-main.28ca6b6a.js"
+  },
+  {
+    "revision": "e76be3a073576b3e267eaa61da542917",
+    "url": "/NewReactPort/static/media/Arch-Rock.e76be3a0.jpg"
+  },
+  {
+    "revision": "f3dbf93f4b6a6bf1181e33fd827f9483",
+    "url": "/NewReactPort/static/media/BudgetTracker.f3dbf93f.png"
+  },
+  {
+    "revision": "337e0b201eb0fd3bc5e7b4fdf5d7bd33",
+    "url": "/NewReactPort/static/media/CoZone.337e0b20.jpg"
+  },
+  {
+    "revision": "313abe21e0147aa4bb7320bb0712d4df",
+    "url": "/NewReactPort/static/media/FitnessTracker.313abe21.png"
+  },
+  {
+    "revision": "1f6192a390320e7299a64c0571b0ddee",
+    "url": "/NewReactPort/static/media/Joshua-Tree.1f6192a3.jpg"
+  },
+  {
+    "revision": "5797f98f8f6efa4003d9d546b805d491",
+    "url": "/NewReactPort/static/media/Listivity.5797f98f.png"
+  },
+  {
+    "revision": "9348063988dd1130698ce8a5d7551fb0",
+    "url": "/NewReactPort/static/media/Monterey.93480639.jpg"
+  },
+  {
+    "revision": "73097e050b8183df45ad7215e550eb2a",
+    "url": "/NewReactPort/static/media/NoteTaker.73097e05.png"
+  },
+  {
+    "revision": "9ffac4b902dab64a1787cad944dbc2c5",
+    "url": "/NewReactPort/static/media/ReactFolio.9ffac4b9.png"
+  }
+]);
